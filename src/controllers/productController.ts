f488c6cd5506bda03819ada69beee8e7e8e38aee
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-09-27 17:18:56 +0530

    feat: created getAdminAllProducts & getSingle, update and delete product API's

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -34,16 +34,14 @@ export const newProduct = TryCatch(
   }
 );
 
-export const getLatestProducts = TryCatch(
-  async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
-    const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+export const getLatestProducts = TryCatch(async (req, res, next) => {
+  const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
 
-    return res.status(200).json({
-      success: true,
-      products,
-    });
-  }
-);
+  return res.status(200).json({
+    success: true,
+    products,
+  });
+});
 
 export const getAllCategories = TryCatch(async (req, res, next) => {
   const categories = await Product.distinct("category");
@@ -51,5 +49,71 @@ export const getAllCategories = TryCatch(async (req, res, next) => {
   return res.status(200).json({
     success: true,
     categories,
+  });
+});
+
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+  const products = await Product.find({});
+
+  return res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) return next(new ErrorHandler("Product not found", 404));
+
+  return res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+export const updateProduct = TryCatch(async (req, res, next) => {
+  const { id } = req.params;
+  const { name, stock, price, category } = req.body;
+  const photo = req.file;
+
+  const product = await Product.findById(id);
+
+  if (!product) return next(new ErrorHandler("Product not found", 404));
+
+  if (photo) {
+    rm(product.photo, () => {
+      console.log("old photo deleted");
+    });
+    product.photo = photo.path;
+  }
+
+  if (name) product.name = name;
+  if (price) product.price = price;
+  if (stock) product.stock = stock;
+  if (category) product.category = category;
+
+  await product.save();
+
+  return res.status(200).json({
+    success: true,
+    message: "Product update successfully",
+  });
+});
+
+export const deleteProduct = TryCatch(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) return next(new ErrorHandler("Product not found", 404));
+
+  rm(product.photo, () => {
+    console.log("photo deleted");
+  });
+
+  await Product.deleteOne();
+
+  return res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
   });
 });
--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { isAdmin } from "../middlewares/authMiddleware.js";
 import {
+  deleteProduct,
+  getAdminProducts,
   getAllCategories,
   getLatestProducts,
+  getSingleProduct,
   newProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { singleUpload } from "../middlewares/multer.js";
 
@@ -16,5 +20,13 @@ app.post("/new", isAdmin, singleUpload, newProduct);
 app.get("/latest", getLatestProducts);
 
 app.get("/categories", getAllCategories);
+
+app.get("/admin-products", isAdmin, getAdminProducts);
+
+app
+  .route("/:id")
+  .get(getSingleProduct)
+  .put(isAdmin, singleUpload, updateProduct)
+  .delete(isAdmin, deleteProduct);
 
 export default app;
