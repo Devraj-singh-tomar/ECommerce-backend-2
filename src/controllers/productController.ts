7472718067c8b4b1fb4a0fa17e96c4d89a2ea65c
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-09-27 16:17:40 +0530

    feat: created getLatestproducts and allcategories API's

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -33,3 +33,23 @@ export const newProduct = TryCatch(
     });
   }
 );
+
+export const getLatestProducts = TryCatch(
+  async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
+    const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+
+    return res.status(200).json({
+      success: true,
+      products,
+    });
+  }
+);
+
+export const getAllCategories = TryCatch(async (req, res, next) => {
+  const categories = await Product.distinct("category");
+
+  return res.status(200).json({
+    success: true,
+    categories,
+  });
+});
--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import { isAdmin } from "../middlewares/authMiddleware.js";
-import { newProduct } from "../controllers/productController.js";
+import {
+  getAllCategories,
+  getLatestProducts,
+  newProduct,
+} from "../controllers/productController.js";
 import { singleUpload } from "../middlewares/multer.js";
 
 const app = express.Router();
 
 // /api/v1/product/new
 app.post("/new", isAdmin, singleUpload, newProduct);
+
+// /api/v1/product/latest
+app.get("/latest", getLatestProducts);
+
+app.get("/categories", getAllCategories);
 
 export default app;
