ca50cc000ad68353045eb7e03e1d1af6e6ccf259
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-09-28 14:48:19 +0530

    feat: created my orders and admin all orders API's

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -4,6 +4,7 @@ import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/orderModel.js";
 import { inValidateCache, reduceStock } from "../utils/features.js";
 import ErrorHandler from "../utils/utilityClass.js";
+import { myCache } from "../app.js";
 
 export const newOrder = TryCatch(
   async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
@@ -51,3 +52,39 @@ export const newOrder = TryCatch(
     });
   }
 );
+
+export const myOrders = TryCatch(async (req, res, next) => {
+  const { id: user } = req.query;
+
+  const key = `my-orders-${user}`;
+
+  let orders = [];
+
+  if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string);
+  else {
+    orders = await Order.find({ user });
+    myCache.set(key, JSON.stringify(orders));
+  }
+
+  return res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+export const allOrders = TryCatch(async (req, res, next) => {
+  const key = `all-orders`;
+
+  let orders = [];
+
+  if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string);
+  else {
+    orders = await Order.find().populate("user", "name");
+
+    myCache.set(key, JSON.stringify(orders));
+  }
+  return res.status(200).json({
+    success: true,
+    orders,
+  });
+});
--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { isAdmin } from "../middlewares/authMiddleware.js";
-import { newOrder } from "../controllers/orderController.js";
+import {
+  allOrders,
+  myOrders,
+  newOrder,
+} from "../controllers/orderController.js";
 
 const app = express.Router();
 
 // /api/v1/order/new
 app.post("/new", newOrder);
+
+app.get("/my", myOrders);
+
+app.get("/all", isAdmin, allOrders);
 
 export default app;
